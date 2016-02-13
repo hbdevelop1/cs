c872e3c7f63bfa97a7eddb8fe896997c89a3e321
hbdevelop1 <hassan17517@gmail.com>
2016-02-13 16:43:13 +1300

    initial client

---
+++ SEClient/SEClient/Window1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SEClient
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+            // Do stuff here, e.g. to the window
+            wnd.Title = "SE Client";
+            // Show the window
+            wnd.Show();
+            //wnd.ShowDialog();
+
+            Close();
+        }
+    }
+}
